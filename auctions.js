3451e07ef4266bb3e1a116a3debf23050e8fa537
RoozbehZhuleh <zhuleh.developer@gmail.com>
2022-04-05 01:25:00 +0430

    prefinal 1
    footer must be responsive.
    JS for other auction elements must be added.
    Menu must be added to Nav items.

---
+++ auctions.js
@@ -0,0 +1,21 @@
+const itemTitle = document.querySelector(".item--right__title__text");
+const itemImage = document.querySelector(".hero__image");
+const likesNumber = document.querySelector(".likes--number");
+const countdown = document.querySelector(".countdown-timer");
+const bidEth = document.querySelector(".bid-eth");
+
+document.querySelector(".auctions__item--1").addEventListener("click", function(){
+    console.log("hello");
+    itemTitle.innerHTML = document.querySelector(".auctions--1__title--text").innerHTML;
+
+    itemImage.setAttribute("src", "./assets/axies-images/03_Live Auctions/card-item8.jpg");
+
+    likesNumber.innerHTML = document.querySelector(".auctions--1--likes").innerHTML;
+});
+
+
+
+
+document.querySelector(".item__wrapper--2").addEventListener("click", function(){
+
+});
